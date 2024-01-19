efd57bf6b3072087c6bf409dc0370ce497af85c0
gucioopielka <gucioopielka@gmail.com>
2024-01-19 20:57:12 +0100

    noise ceilings

--- perform_brain_RSA.py
+++ perform_brain_RSA.py
@@ -16,6 +16,28 @@ os.environ["VECLIB_MAXIMUM_THREADS"] = n_cores
 os.environ["NUMEXPR_NUM_THREADS"] = n_cores
 
 
+def compute_noise_ceilings(rdms):
+    """Compute the lower and upper noise ceilings for a collection of RDMs."""
+    
+    lower_noise_ceilings = []
+    upper_noise_ceilings = []
+    
+    for i in range(len(rdms)):
+        # Exclude the i-th RDM
+        training_rdms = [rdms[j] for j in range(len(rdms)) if j != i]
+        test_rdm = rdms[i]
+
+        # Lower noise ceiling: Mean RDM without the test RDM
+        mean_training_rdm = np.mean(training_rdms, axis=0)
+        lower_noise_ceilings.append(compute_RSA(mean_training_rdm, test_rdm))
+
+        # Upper noise ceiling: Mean RDM including the test RDM
+        mean_all_rdm = np.mean(rdms, axis=0)
+        upper_noise_ceilings.append(compute_RSA(mean_all_rdm, test_rdm))
+        
+    return np.mean(lower_noise_ceilings), np.mean(upper_noise_ceilings)
+
+
 # Load NSD info
 nsd_info_shared = pd.read_csv('/home/c12049018/nsd_shared_imgs.csv', dtype={'cocoId': str, 'nsdId': str})
 # Load COCO caption embeddings
@@ -50,6 +72,7 @@ sem_rdm = get_RDM(caption_embeddings)
 ### Compute FMRI-SALIENCY/SEMANTIC RSA
 fmri_sal_RSA = np.zeros((7, 32))
 fmri_sem_RSA = np.zeros((7, 32))
+roi_rdms = np.zeros((7, 32, len(nsd_info_shared), len(nsd_info_shared)))
 
 for sub_idx, sub in enumerate([1, 2, 3, 4, 5, 6, 8]):
     print(f'Subject : {sub}')
@@ -65,14 +88,26 @@ for sub_idx, sub in enumerate([1, 2, 3, 4, 5, 6, 8]):
         if (roi[0].shape[1] == 0) or (roi[1].shape[1] == 0): # If no data in one of the hemis for this ROI
             fmri_sal_RSA[sub_idx, roi_idx] = np.nan
             fmri_sem_RSA[sub_idx, roi_idx] = np.nan
+            roi_rdms[sub_idx, roi_idx, :, :] = np.nan
 
         else:
             rdm_l_hemi, rdm_r_hemi = get_RDM(roi[0]), get_RDM(roi[1])
             roi_rdm = np.mean(np.stack((rdm_l_hemi, rdm_r_hemi), axis=0), axis=0) # Avg across hemis
             fmri_sal_RSA[sub_idx, roi_idx] = compute_RSA(roi_rdm, sal_rdm)
             fmri_sem_RSA[sub_idx, roi_idx] = compute_RSA(roi_rdm, sem_rdm)
+            roi_rdms[sub_idx, roi_idx, :, :] = roi_rdm
+
+
+### Compute noise ceilings
+roi_noise_ceilings = np.zeros((32, 2))
+for roi_idx in range(32):
+    try:
+        roi_noise_ceilings[roi_idx, :] = compute_noise_ceilings(roi_rdms[:, roi_idx, :, :])
+    except:
+        roi_noise_ceilings[roi_idx, :] = np.nan
 
 
 # Save RSA results
 np.save('data/fmri_sal_RSA', fmri_sal_RSA)
 np.save('data/fmri_sem_RSA', fmri_sem_RSA)
+np.save('data/roi_noise_ceilings', roi_noise_ceilings)
